af6c67e2e2b1fbc978d0c2812ed1eb3187009a39
LINAN1345272421 <1345272421@qq.com>
2021-04-15 19:58:58 +0800

    update

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def update_all():
     utils.update_details()
     return "1"
 
+@app.route('/all_world')
+def update_all_world():
+    utils.update_history()
+    utils.update_details()
+    utils.update_world()
+    return "1"
+
 @app.route('/tem')
 def hello_world():
     return render_template("index.html")
@@ -19,7 +26,16 @@ def hello_world():
 @app.route('/c1')
 def get_c1_data():
     data=utils.get_c1_data()
+    print("中国统计")
+    print(data)
     return jsonify({"confirm":data[0],"suspect":data[1],"heal":data[2],"dead":data[3]})
+
+@app.route('/world_c1')
+def get_world_c1_data():
+    data=utils.get_world_c1_data()
+    print("世界统计:")
+    print(data)
+    return jsonify({"confirm":data[0],"heal":data[1],"dead":data[2],"nowConfirm":data[3]})
 
 @app.route('/c2')
 def get_c2_data():
@@ -33,6 +49,7 @@ def get_c2_data():
         res=[]
         for tup in utils.get_city_data(delname(name)):
             res.append({"name":addcityname(tup[0]),"value":int(tup[1]),"heal": int(tup[2]),"dead": int(tup[3])})
+    print("下砖数据")
     print(res)
     return jsonify({"data":res})
 
@@ -99,6 +116,8 @@ def get_r1_data():
     list=[]
     for a,b, in data:
         list.append({"name":a,"value":b})
+    print("非湖北地区TOP5")
+    print(list)
     return jsonify({"data":list})
 
 @app.route("/r2")
@@ -107,8 +126,34 @@ def get_r2_data():
     list=[]
     for a,b, in data:
         list.append({"name":a,"value":b})
+    print("境外输入TOP5")
+    print(list)
     return jsonify({"data":list})
 
+@app.route("/world_bar")
+def get_world_bar_data():
+    data = utils.get_world_bar_data()
+    list=[]
+    name=[]
+    value=[]
+    for a,b, in data:
+        list.append({"name":a,"value":b})
+        name.append(a)
+        value.append(b)
+    print("世界确诊TOP8")
+    print(list)
+    return jsonify({"name":name,"value":value})
+
+@app.route("/all_world_data")
+def get_all_world_data():
+    data=[]
+    for a in utils.get_all_world_data():
+        data.append({"name":trans_world(a[0]),"value":a[1],"heal":a[2],"dead":a[3]})
+    i=utils.get_china_data()
+    data.append({"name":"China","value":i[1],"heal":i[2],"dead":i[3]})
+    print("世界地图数据")
+    print(data)
+    return jsonify({"data":data})
 
 def addname(name):
     list=["北京","天津","上海","重庆"]
@@ -230,7 +275,435 @@ def addcityname(name):
         return name
     if(name[len(name)-1]=='县'):
         return name
+    if(name[len(name)-1]=='市'):
+        return name
     return name+"市"
 
+def trans_world(name):
+    if(name=="阿富汗"):
+        return "Afghanistan"
+    if(name=="安哥拉"):
+        return "Angola"
+    if(name=="阿尔巴尼亚"):
+        return "Albania"
+    if(name=="阿联酋"):
+        return "United Arab Emirates"
+    if(name=="阿根廷"):
+        return "Argentina"
+    if(name=="亚美尼亚"):
+        return "Armenia"
+    if(name=="法属南半球和南极领地"):
+        return "French Southern and Antarctic Lands"
+    if(name=="澳大利亚"):
+        return "Australia"
+    if(name=="奥地利"):
+        return "Austria"
+    if(name=="阿塞拜疆"):
+        return "Azerbaijan"
+    if(name=="布隆迪"):
+        return "Burundi"
+    if(name=="比利时"):
+        return "Belgium"
+    if(name=="贝宁"):
+        return "Benin"
+    if(name=="布基纳法索"):
+        return "Burkina Faso"
+    if(name=="孟加拉国"):
+        return "Bangladesh"
+    if(name=="保加利亚"):
+        return "Bulgaria"
+    if(name=="巴哈马"):
+        return "The Bahamas"
+    if(name=="波黑"):
+        return "Bosnia and Herz."
+    if(name=="白俄罗斯"):
+        return "Belarus"
+    if(name=="伯利兹"):
+        return "Belize"
+    if(name=="百慕大"):
+        return "Bermuda"
+    if(name=="玻利维亚"):
+        return "Bolivia"
+    if(name=="巴西"):
+        return "Brazil"
+    if(name=="文莱"):
+        return "Brunei"
+    if(name=="不丹"):
+        return "Bhutan"
+    if(name=="博茨瓦纳"):
+        return "Botswana"
+    if(name=="加拿大"):
+        return "Canada"
+    if(name=="瑞士"):
+        return "Switzerland"
+    if(name=="智利"):
+        return "Chile"
+    if(name=="中国"):
+        return "China"
+    if(name=="象牙海岸"):
+        return "Ivory Coast"
+    if(name=="喀麦隆"):
+        return "Cameroon"
+    if(name=="刚果（金）"):
+        return "Dem. Rep. Congo"
+    if(name=="刚果（布）"):
+        return "Congo"
+    if(name=="哥伦比亚"):
+        return "Colombia"
+    if(name=="哥斯达黎加"):
+        return "Costa Rica"
+    if(name=="古巴"):
+        return "Cuba"
+    if(name=="北塞浦路斯"):
+        return "Northern Cyprus"
+    if(name=="塞浦路斯"):
+        return "Cyprus"
+    if(name=="捷克"):
+        return "Czech Rep."
+    if(name=="德国"):
+        return "Germany"
+    if(name=="吉布提"):
+        return "Djibouti"
+    if(name=="丹麦"):
+        return "Denmark"
+    if(name=="多米尼加共和国"):
+        return "Dominican Republic"
+    if(name=="阿尔及利亚"):
+        return "Algeria"
+    if(name=="厄瓜多尔"):
+        return "Ecuador"
+    if(name=="埃及"):
+        return "Egypt"
+    if(name=="厄立特里亚"):
+        return "Eritrea"
+    if(name=="西班牙"):
+        return "Spain"
+    if(name=="爱沙尼亚"):
+        return "Estonia"
+    if(name=="埃塞俄比亚"):
+        return "Ethiopia"
+    if(name=="芬兰"):
+        return "Finland"
+    if(name=="斐"):
+        return "Fiji"
+    if(name=="福克兰群岛"):
+        return "Falkland Islands"
+    if(name=="法国"):
+        return "France"
+    if(name=="加蓬"):
+        return "Gabon"
+    if(name=="英国"):
+        return "United Kingdom"
+    if(name=="格鲁吉亚"):
+        return "Georgia"
+    if(name=="加纳"):
+        return "Ghana"
+    if(name=="几内亚"):
+        return "Guinea"
+    if(name=="冈比亚"):
+        return "Gambia"
+    if(name=="几内亚比绍"):
+        return "Guinea-Bissau"
+    if(name=="赤道几内亚"):
+        return "Eq. Guinea"
+    if(name=="希腊"):
+        return "Greece"
+    if(name=="格陵兰"):
+        return "Greenland"
+    if(name=="危地马拉"):
+        return "Guatemala"
+    if(name=="法属圭亚那"):
+        return "French Guiana"
+    if(name=="圭亚那"):
+        return "Guyana"
+    if(name=="洪都拉斯"):
+        return "Honduras"
+    if(name=="克罗地亚"):
+        return "Croatia"
+    if(name=="海地"):
+        return "Haiti"
+    if(name=="匈牙利"):
+        return "Hungary"
+    if(name=="印度尼西亚"):
+        return "Indonesia"
+    if(name=="印度"):
+        return "India"
+    if(name=="爱尔兰"):
+        return "Ireland"
+    if(name=="伊朗"):
+        return "Iran"
+    if(name=="伊拉克"):
+        return "Iraq"
+    if(name=="冰岛"):
+        return "Iceland"
+    if(name=="以色列"):
+        return "Israel"
+    if(name=="意大利"):
+        return "Italy"
+    if(name=="牙买加"):
+        return "Jamaica"
+    if(name=="约旦"):
+        return "Jordan"
+    if(name=="日本本土"):
+        return "Japan"
+    if(name=="哈萨克斯坦"):
+        return "Kazakhstan"
+    if(name=="肯尼亚"):
+        return "Kenya"
+    if(name=="吉尔吉斯斯坦"):
+        return "Kyrgyzstan"
+    if(name=="柬埔寨"):
+        return "Cambodia"
+    if(name=="科索沃"):
+        return "Kosovo"
+    if(name=="科威特"):
+        return "Kuwait"
+    if(name=="老挝"):
+        return "Laos"
+    if(name=="黎巴嫩"):
+        return "Lebanon"
+    if(name=="利比里亚"):
+        return "Liberia"
+    if(name=="利比亚"):
+        return "Libya"
+    if(name=="斯里兰卡"):
+        return "Sri Lanka"
+    if(name=="莱索托"):
+        return "Lesotho"
+    if(name=="立陶宛"):
+        return "Lithuania"
+    if(name=="卢森堡"):
+        return "Luxembourg"
+    if(name=="拉脱维亚"):
+        return "Latvia"
+    if(name=="摩洛哥"):
+        return "Morocco"
+    if(name=="摩尔多瓦"):
+        return "Moldova"
+    if(name=="马达加斯加"):
+        return "Madagascar"
+    if(name=="墨西哥"):
+        return "Mexico"
+    if(name=="北马其顿"):
+        return "Macedonia"
+    if(name=="马里"):
+        return "Mali"
+    if(name=="缅甸"):
+        return "Myanmar"
+    if(name=="黑山"):
+        return "Montenegro"
+    if(name=="蒙古"):
+        return "Mongolia"
+    if(name=="莫桑比克"):
+        return "Mozambique"
+    if(name=="毛里塔尼亚"):
+        return "Mauritania"
+    if(name=="马拉维"):
+        return "Malawi"
+    if(name=="马来西亚"):
+        return "Malaysia"
+    if(name=="纳米比亚"):
+        return "Namibia"
+    if(name=="新喀里多尼亚"):
+        return "New Caledonia"
+    if(name=="尼日尔"):
+        return "Niger"
+    if(name=="尼日利亚"):
+        return "Nigeria"
+    if(name=="尼加拉瓜"):
+        return "Nicaragua"
+    if(name=="荷兰"):
+        return "Netherlands"
+    if(name=="挪威"):
+        return "Norway"
+    if(name=="尼泊尔"):
+        return "Nepal"
+    if(name=="新西兰"):
+        return "New Zealand"
+    if(name=="阿曼"):
+        return "Oman"
+    if(name=="巴基斯坦"):
+        return "Pakistan"
+    if(name=="巴拿马"):
+        return "Panama"
+    if(name=="秘鲁"):
+        return "Peru"
+    if(name=="菲律宾"):
+        return "Philippines"
+    if(name=="巴布亚新几内亚"):
+        return "Papua New Guinea"
+    if(name=="波兰"):
+        return  "Poland"
+    if(name=="波多黎各"):
+        return "Puerto Rico"
+    if(name=="葡萄牙"):
+        return "Portugal"
+    if(name=="巴拉圭"):
+        return "Paraguay"
+    if(name=="卡塔尔"):
+        return "Qatar"
+    if(name=="罗马尼亚"):
+        return "Romania"
+    if(name=="俄罗斯"):
+        return "Russia"
+    if(name=="卢旺达"):
+        return "Rwanda"
+    if(name=="西撒哈拉"):
+        return "W. Sahara"
+    if(name=="沙特阿拉伯"):
+        return "Saudi Arabia"
+    if(name=="苏丹"):
+        return "Sudan"
+    if(name=="南苏丹"):
+        return "South Sudan"
+    if(name=="塞内加尔"):
+        return "Senegal"
+    if(name=="所罗门群岛"):
+        return "Solomon Islands"
+    if(name=="塞拉利昂"):
+        return "Sierra Leone"
+    if(name=="萨尔瓦多"):
+        return "El Salvador"
+    if(name=="索马里兰"):
+        return "Somaliland"
+    if(name=="索马里"):
+        return "Somalia"
+    if(name=="塞尔维亚"):
+        return "Serbia"
+    if(name=="苏里南"):
+        return "Suriname"
+    if(name=="斯洛伐克"):
+        return "Slovakia"
+    if(name=="斯洛文尼亚"):
+        return "Slovenia"
+    if(name=="瑞典"):
+        return "Sweden"
+    if(name=="斯威士兰"):
+        return "Swaziland"
+    if(name=="叙利亚"):
+        return "Syria"
+    if(name=="乍得"):
+        return "Chad"
+    if(name=="多哥"):
+        return "Togo"
+    if(name=="泰国"):
+        return "Thailand"
+    if(name=="塔吉克斯坦"):
+        return "Tajikistan"
+    if(name=="土库曼斯坦"):
+        return "Turkmenistan"
+    if(name=="东帝汶"):
+        return "East Timor"
+    if(name=="特里尼达和多巴哥"):
+        return "Trinidad and Tobago"
+    if(name=="突尼斯"):
+        return "Tunisia"
+    if(name=="土耳其"):
+        return "Turkey"
+    if(name=="乌干达"):
+        return "Uganda"
+    if(name=="乌克兰"):
+        return "Ukraine"
+    if(name=="乌拉圭"):
+        return "Uruguay"
+    if(name=="美国"):
+        return "United States"
+    if(name=="乌兹别克斯坦"):
+        return "Uzbekistan"
+    if(name=="委内瑞拉"):
+        return "Venezuela"
+    if(name=="越南"):
+        return "Vietnam"
+    if(name=="瓦努阿图"):
+        return  "Vanuatu"
+    if(name=="西岸"):
+        return "West Bank"
+    if(name=="也门"):
+        return "Yemen"
+    if(name=="南非"):
+        return "South Africa"
+    if(name=="赞比亚"):
+        return "Zambia"
+    if(name=="津巴布韦"):
+        return "Zimbabwe"
+    if(name=="新加坡"):
+        return "Singapore Rep."
+    if(name=="多米尼加"):
+        return "Dominican Rep."
+    if(name=="巴勒斯坦"):
+        return "Palestine"
+    if(name=="巴哈马"):
+        return "Bahamas"
+    if(name=="东帝汶"):
+        return "Timor-Leste"
+    if(name=="几内亚比绍"):
+        return "Guinea-Bissau"
+    if(name=="科特迪瓦"):
+        return "Côte d'Ivoire"
+    if(name=="锡亚琴冰川"):
+        return "Siachen Glacier"
+    if(name=="英属印度洋领土"):
+        return "Br. Indian Ocean Ter."
+    if(name=="波斯尼亚和黑塞哥维那"):
+        return "Bosnia and Herz."
+    if(name=="中非共和国"):
+        return "Central African Rep."
+    if(name=="北塞浦路斯"):
+        return "N. Cyprus"
+    if(name=="捷克"):
+        return "Czech Rep."
+    if(name=="韩国"):
+        return "Korea"
+    if(name=="老挝"):
+        return "Lao PDR"
+    if(name=="朝鲜"):
+        return "Dem. Rep. Korea"
+    if(name=="西撒哈拉"):
+        return "W. Sahara"
+    if(name=="南苏丹"):
+        return "S. Sudan"
+    if(name=="所罗门群岛"):
+        return "Solomon Is."
+    if(name=="塞尔维亚"):
+        return "Serbia"
+    if(name=="坦桑尼亚"):
+        return "Tanzania"
+
+
+
+
+@app.route('/tem2')
+def hello_world2():
+    return render_template("index2.html")
+
+@app.route('/query2')
+def get_query_data2():
+    fromyear=request.values.get("fromyear")
+    frommounth=request.values.get("frommounth")
+    fromday=request.values.get("fromday")
+    toyear=request.values.get("toyear")
+    tomounth=request.values.get("tomounth")
+    today=request.values.get("today")
+
+    continent=request.values.get("continent")
+    country=request.values.get("country")
+    province=request.values.get("province")
+    city=request.values.get("city")
+    Data=[]
+    if(fromday!=""):
+        for data in \
+                utils.get_query_like1(fromyear,frommounth,fromday,toyear,tomounth,today,
+                    continent,country,province,city):
+            Data.append(data)
+    else:
+        for data in \
+                utils.get_query_like2(continent,country,province,city):
+            Data.append(data)
+    return jsonify({"data":Data})
+
+@app.route('/tem3')
+def hello_world_android():
+    return render_template("android.html")
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ from flask import json
 
 def get_conn():
     """
-    :return: 连接，游标
+    :return: 连接，游标192.168.1.102
     """
     # 创建连接
-    conn = pymysql.connect(host="127.0.0.1",
+    conn = pymysql.connect(host="10.92.5.115",
                     user="root",
                     password="123456",
                     db="yiqing",
@@ -19,7 +19,6 @@ def get_conn():
     # 创建游标
     cursor = conn.cursor()  # 执行完毕返回的结果集默认以元组显示
     return conn, cursor
-
 
 def close_conn(conn, cursor):
     if cursor:
@@ -35,7 +34,7 @@ def query(sql,*args):
     :return: 返回查询结果以((),(),)形式
     """
     conn,cursor = get_conn();
-    cursor.execute(sql,args)
+    cursor.execute(sql)
     res=cursor.fetchall()
     close_conn(conn,cursor)
     return res
@@ -66,32 +65,6 @@ def update_details():
     finally:
         close_conn(conn, cursor)
 
-def insert_history():
-    """
-        插入历史数据
-    :return:
-    """
-    cursor = None
-    conn = None
-    try:
-        dic = get_tencent_data()[0]  # 0 是历史数据字典,1 最新详细数据列表
-        print(f"{time.asctime()}开始插入历史数据")
-        conn, cursor = get_conn()
-        sql = "insert into history values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        for k, v in dic.items():
-            # item 格式 {'2020-01-13': {'confirm': 41, 'suspect': 0, 'heal': 0, 'dead': 1}
-            cursor.execute(sql, [k, v.get("confirm"), v.get("confirm_add"), v.get("suspect"),
-                                 v.get("suspect_add"), v.get("heal"), v.get("heal_add"),
-                                 v.get("dead"), v.get("dead_add")])
-
-        conn.commit()  # 提交事务 update delete insert操作
-        print(f"{time.asctime()}插入历史数据完毕")
-    except:
-        traceback.print_exc()
-    finally:
-        close_conn(conn, cursor)
-
-
 def update_history():
     """
     更新历史数据
@@ -118,6 +91,57 @@ def update_history():
     finally:
         close_conn(conn, cursor)
 
+def insert_history():
+    """
+        插入历史数据
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        dic = get_tencent_data()[0]  # 0 是历史数据字典,1 最新详细数据列表
+        print(f"{time.asctime()}开始插入历史数据")
+        conn, cursor = get_conn()
+        sql = "insert into history values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        for k, v in dic.items():
+            # item 格式 {'2020-01-13': {'confirm': 41, 'suspect': 0, 'heal': 0, 'dead': 1}
+            cursor.execute(sql, [k, v.get("confirm"), v.get("confirm_add"), v.get("suspect"),
+                                 v.get("suspect_add"), v.get("heal"), v.get("heal_add"),
+                                 v.get("dead"), v.get("dead_add")])
+
+        conn.commit()  # 提交事务 update delete insert操作
+        print(f"{time.asctime()}插入历史数据完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+
+def update_world():
+    """
+    更新 world 表
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        li = get_tencent_data()[2]  #  0 是历史数据字典,1 最新详细数据列表,2世界数据
+        conn, cursor = get_conn()
+        sql = "insert into world(update_time,country,continent,confirmAdd,confirm,suspect,dead,heal,nowConfirm) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        sql_query = 'select %s=(select update_time from world order by id desc limit 1)' #对比当前最大时间戳
+        cursor.execute(sql_query,li[0][0])
+        print(li[0][0])
+        if not cursor.fetchone()[0]:
+            print(f"{time.asctime()}开始更新最新数据(world)")
+            for item in li:
+                cursor.execute(sql, item)
+            conn.commit()  # 提交事务 update delete insert操作
+            print(f"{time.asctime()}更新最新数据完毕(world)")
+        else:
+            print(f"{time.asctime()}已是最新数据！(world)")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
 
 def get_tencent_data():
     """
@@ -125,6 +149,7 @@ def get_tencent_data():
     """
     url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5'
     url_his = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_other'  # 加上这个history大兄弟++++++++
+    url_world ='https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist'
 
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
@@ -137,6 +162,11 @@ def get_tencent_data():
     r_his = requests.get(url_his, headers)
     res_his = json.loads(r_his.text)
     data_his = json.loads(res_his['data'])
+
+    #world数据
+    r_world = requests.get(url_world, headers)
+    res_world = json.loads(r_world.text)
+    data_world = res_world['data']
 
     history = {}  # 历史数据
     #     for i in data_all["chinaDayList"]:
@@ -192,7 +222,14 @@ def get_tencent_data():
             heal = city_infos["total"]["heal"]
             dead = city_infos["total"]["dead"]
             details.append([update_time, province, city, confirm, confirm_add, heal, dead])
-    return history, details
+    world=[]
+    for i in data_world:
+        ds = i['y']+"."+i['date']
+        tup = time.strptime(ds, "%Y.%m.%d")
+        ds = time.strftime("%Y-%m-%d", tup)
+        world.append([ds,i['name'],i['continent'],i['confirmAdd'],i['confirm'],i['suspect'],i['dead'],i['heal'],i['nowConfirm']])
+
+    return history, details,world
 
 def get_c1_data():
     """
@@ -206,7 +243,20 @@ def get_c1_data():
         "from details "\
         "where update_time=(select update_time from details order by update_time desc limit 1)"
     res=query(sql)
-    print(res[0])
+    return res[0]
+
+def get_world_c1_data():
+    """
+
+    :return: 大屏主数据
+    """
+    sql="select sum(confirm),"\
+        "sum(heal), "\
+        "sum(dead), "\
+        "sum(nowConfirm) "\
+        "from world "\
+        "where update_time=(select update_time from world order by update_time desc limit 1)"
+    res=query(sql)
     return res[0]
 
 def get_c2_data():
@@ -219,7 +269,6 @@ def get_c2_data():
           "order by update_time desc limit 1) " \
           "group by province"
     res = query(sql)
-    print(res)
     return res
 
 
@@ -233,7 +282,6 @@ def get_b1_data():
           "order by update_time desc limit 1) " \
           "group by province"
     res = query(sql)
-    print(res)
     return res
 
 def get_query_data(year,month,day):
@@ -302,7 +350,6 @@ def get_r1_data():
         'and province in ("北京","上海","天津","重庆") group by province) as a '\
         'order by confirm desc limit 5'
     res=query(sql)
-    print(res)
     return res
 
 def get_city_data(name):
@@ -310,15 +357,101 @@ def get_city_data(name):
         'where update_time=(select update_time from details order by update_time desc limit 1) '\
         'and province='+'"'+name+'"'+' and city not in ("境外输入","地区待确认") '
     res=query(sql)
-    print(res)
     return res
 
 def get_r2_data():
     sql='select province,confirm from details where update_time=(select update_time from details ' \
           'order by update_time desc limit 1) and city="境外输入" order by confirm desc limit 5'
     res=query(sql)
+    return res
+
+def get_world_bar_data():
+    sql='select country,confirm from world where update_time=(select update_time from world ' \
+          'order by update_time desc limit 1) order by confirm desc limit 8'
+    res=query(sql)
+    return res
+
+def get_all_world_data():
+    sql='select country,confirm,heal,dead from world where update_time=(select update_time from world ' \
+          'order by update_time desc limit 1) '
+    res=query(sql)
+    return res
+
+def get_china_data():
+    sql='select confirm,heal,dead from history where ds=(select update_time from world ' \
+          'order by ds desc limit 1) limit 1'
+    res=query(sql)
+    Res=('中国',res[0][0],res[0][1],res[0][2])
+    return Res
+
+def get_query_like1(fromyear,frommonth,fromday,toyear,tomonth,today,
+                    continent,country,province,city):
+    Year=str(fromyear)
+    Month=str(frommonth)
+    Day=str(fromday)
+    ds = Year+"." + Month+"."+Day
+    print(ds)
+    tup = time.strptime(ds, "%Y.%m.%d")
+    print(tup)
+    ds = time.strftime("%Y-%m-%d", tup)
+    Year=str(toyear)
+    Month=str(tomonth)
+    Day=str(today)
+    ds1 = Year+"." + Month+"."+Day
+    print(ds1)
+    tup1 = time.strptime(ds1, "%Y.%m.%d")
+    print(tup1)
+    ds1 = time.strftime("%Y-%m-%d", tup1)
+    if(province!="" or city!="" or country=="中国"):
+        sql = "select update_time,province,sum(confirm),sum(heal),sum(dead) from details " \
+              "where (update_time between " + "'" + ds + "' and '" + ds1 + "'" + ") "
+        if (province != "" and city ==""):
+            sql="select update_time,province,city,confirm,heal,dead from details " \
+              "where (update_time between " + "'" + ds + "' and '" + ds1 + "'" + ") "
+            sql = sql + "and (province like " + '"%' + province + '%") '
+        if (province!="" and city != ""):
+            sql = "select update_time,province,city,confirm,heal,dead from details " \
+                  "where (update_time between " + "'" + ds + "' and '" + ds1 + "'" + ") "
+            sql = sql + "and (city like " + '"%' + city + '%") '
+            sql = sql + "and (province like " + '"%' + province + '%") '
+        if(country=="中国" and province=="" and city==""):
+            sql=sql+"group by  province , update_time"
+    if(province=="" and city=="" and country!="中国"):
+        sql = "select update_time,country,confirm,heal,dead,nowconfirm from world " \
+              "where (update_time between " + "'" + ds + "' and '" + ds1 + "'" + ") "
+        if (continent != ""):
+            sql = sql + "and (continent like " + '"%' + continent + '%") '
+        if (country != ""):
+            sql = sql + "and (country like " + '"%' + country + '%") '
+    print("查询（带日期）")
+    print(sql)
+    res = query(sql)
+    print(res)
+    return res
+
+def get_query_like2(continent,country,province,city):
+    if(province!="" or city!="" or country=="中国"):
+        sql = "select update_time,province,sum(confirm),sum(heal),sum(dead) from details where 1=1 "
+        if (province != "" and city==""):
+            sql="select update_time,province,city,confirm,heal,dead from details where 1=1 "
+            sql = sql + "and (province like " + '"%' + province + '%") '
+        if (city != "" and province !=""):
+            sql = "select update_time,province,city,confirm,heal,dead from details where 1=1 "
+            sql = sql + "and (city like " + '"%' + city + '%") '
+            sql=sql+"and (province like " + '"%' + province + '%") '
+        if(country=="中国" and province=="" and city==""):
+            sql=sql+"group by  province , update_time"
+    if(province=="" and city=="" and country!="中国"):
+        sql = "select update_time,country,confirm,heal,dead,nowconfirm from world where 1=1 "
+        if (continent != ""):
+            sql = sql + "and (continent like " + '"%' + continent + '%") '
+        if (country != ""):
+            sql = sql + "and (country like " + '"%' + country + '%") '
+    print(sql)
+    print("查询（无日期）")
+    res = query(sql)
     print(res)
     return res
 
 if __name__ == "__main__":
-    update_details()
+    update_world()
